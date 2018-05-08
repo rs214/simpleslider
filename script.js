dcc1e07b174534952be1c177ebeb0af995e9fe3f
rs214 <robertsi0214@gmail.com>
2018-05-08 15:44:10 -0700

    implement auto and manual feature into one slider

--- script.js
+++ script.js
@@ -1,52 +1,40 @@
-//Right and Left Arrow functionality
-let sliderImages = document.querySelectorAll('.slide'),
-    arrowRight = document.querySelector('#arrow-right'),
-    arrowLeft = document.querySelector('#arrow-left'),
-    current = 0;
+var slideIndex = 0;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
 
+showSlides();
 
-function clear() {
-  for (let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.display = 'none';
+function showSlides() {
+    var i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 8000); // Change image every 5 seconds
+}
+
+function currentSlide(n) {
+    var i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex = n;
+    slides[n-1].style.display = "block";
+    dots[n-1].className += " active";
+}
+
+function plusSlides(n) {
+  var newslideIndex = slideIndex + n;
+  if(newslideIndex < 4 && newslideIndex > 0){
+     currentSlide(newslideIndex);
   }
 }
-
-function start() {
-  //clear all content
-  clear();
-  //initialize slider
-  sliderImages[0].style.display = 'block';
-}
-
-function slideLeft() {
-  clear();
-  sliderImages[current-1].style.display = 'block';
-  current--;
-}
-
-function slideRight() {
-  clear();
-  sliderImages[current+1].style.display = 'block';
-  current++;
-}
-
-arrowLeft.addEventListener('click', function () {
-  if (current === 0) {
-    current = sliderImages.length;
-  }
-  slideLeft();
-});
-
-arrowRight.addEventListener('click', function () {
-  if (current === sliderImages.length - 1) {
-    current = -1;
-  }
-  slideRight();
-});
-
-start();
-
-
-//Autoslide
-
-
